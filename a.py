8496989c807cc1de5c25c01da1698da863fdd387
ekusiadadus <ekusiadadus@gmail.com>
2023-02-21 15:58:25 +0900

    feat: :fire: add comments

--- a.py
+++ a.py
@@ -1,15 +1,30 @@
+
+# Import libraries
 import optuna
 
-def objective(trial):
+# define objective function to minimize
+
+
+def objective(trial: optuna.trial.Trial) -> float:
     x = trial.suggest_float("x", -4.5, 4.5)
     y = trial.suggest_float("y", -4.5, 4.5)
+
+    print(f"x: {x}, y: {y}, objective: {(1.5 - x + x*y) ** 2 + (2.25 - x + x * y ** 2) ** 2 + (2.625 - x + x * y ** 3) ** 2}")
 
     return (1.5 - x + x*y) ** 2 + (2.25 - x + x * y ** 2) ** 2 + (2.625 - x + x * y ** 3) ** 2
 
 
-study = optuna.create_study(direction="minimize")
-study.optimize(objective, n_trials=1000)
+def main() -> None:
+    # create study object and optimize
+    study: optuna.study.Study = optuna.create_study(direction="minimize")
+    # study.optimize(objective, n_trials=1000)
+    # study.optimize(objective, n_trials=1000, catch=(TypeError, ValueError))
+    study.optimize(objective, n_trials=1000, catch=Exception)
 
-print(f"Best objective value: {study.best_value}")
-print(f"Best parameter: {study.best_params}")
+    # print results
+    print(f"Best objective value: {study.best_value}")
+    print(f"Best parameter: {study.best_params}")
 
+
+if __name__ == "__main__":
+    main()
